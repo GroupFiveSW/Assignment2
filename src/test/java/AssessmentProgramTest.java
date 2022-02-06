bd5066692a7f439d063eabb9e07af90b109d9c4e
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2022-02-06 17:40:14 +0100

    feat/#17. Adds three unit tests for AssessmentProgram

--- src/test/java/AssessmentProgramTest.java
+++ src/test/java/AssessmentProgramTest.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class AssessmentProgramTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssessmentProgramTest {
+
+    @Test
+    void addNumTest(){
+        int a = 5;
+        int b = 4;
+        assertEquals(9, AssessmentProgram.addNum(a,b), "addNumTest returned wrong result");
+    }
+
+    @Test
+    void addNumHighNumbersTest(){
+        int a = 200;
+        int b = 1000;
+        assertEquals(1200, AssessmentProgram.addNum(a,b), "addNumHighNumbersTest returned wrong result");
+
+    }
+
+    @Test
+    void multiplyNum(){
+        int a = 2;
+        int b = 3;
+        assertEquals(6, AssessmentProgram.multiplyNum(a,b), "multiplyNum returned wrong result");
+
+    }
+
+
 }
