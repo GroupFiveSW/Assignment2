f89fb7c8a14749311a4f94ad52efa18a9debf450
Alexander Krantz <klako@hotmail.se>
2022-02-08 19:09:39 +0100

    MavenIntegration now uses different java homes depending on settings

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Assignment2
 Assignment2
+
+## How to run
+In order to test the project, run `mvn test`
+
+In order for the maven compile and test functionalities to function correctly, one of the following actions must be taken. 
+1. Set the `JAVA_HOME` environment variable to the location of a JDK.
+2. Set the `MAVENINTGR_JAVAHOME` environment variable to the location of a JDK.
+3. Copy the `user.properties.template` to `user.properties`and set the `mavenintgr.java_home` property to the location of a JDK
--- src/test/java/MavenIntegrationTest.java
+++ src/test/java/MavenIntegrationTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,9 +10,10 @@ class MavenIntegrationTest {
      */
     @Test
     void testTestTrue() {
+
         var mvn = new MavenIntegration("src/test/testprojects/valid/pom.xml");
         var result = assertDoesNotThrow(()->mvn.test(), "Test should not throw error");
-        assertFalse(result.isSuccessful(), "Test should be successful");
+        assertTrue(result.isSuccessful(), "Test should be successful");
     }
 
     /**
@@ -22,6 +24,6 @@ class MavenIntegrationTest {
         var mvn = new MavenIntegration("src/test/testprojects/testfail/pom.xml");
         var result = assertDoesNotThrow(()->mvn.test(), "Test should not throw error");
         assertFalse(result.isSuccessful(), "Test should not be successful");
-        assertFalse(result.getLog().contains("Tests run: 1, Failures: 1"), "Test should fail 1 out of 1 tests");
+        assertTrue(result.getLog().contains("Tests run: 1, Failures: 1"), "Test should fail 1 out of 1 tests");
     }
 }
